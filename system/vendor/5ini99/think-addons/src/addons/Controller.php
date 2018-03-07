964e9d611f1f6b085bbd1e0d054c56a824480bd9
顾阳 <38082270@qq.com>
2018-03-07 17:23:21 +0800

    ...
    Signed-off-by: 顾阳 <38082270@qq.com>

---
+++ system/vendor/5ini99/think-addons/src/addons/Controller.php
@@ -0,0 +1,99 @@
+<?php
+// +----------------------------------------------------------------------
+// | thinkphp5 Addons [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Byron Sampson <xiaobo.sun@qq.com>
+// +----------------------------------------------------------------------
+namespace think\addons;
+
+use think\Request;
+use think\Config;
+use think\Loader;
+
+/**
+ * 插件基类控制器
+ * Class Controller
+ * @package think\addons
+ */
+class Controller extends \think\Controller
+{
+    // 当前插件操作
+    protected $addon = null;
+    protected $controller = null;
+    protected $action = null;
+    // 当前template
+    protected $template;
+    // 模板配置信息
+    protected $config = [
+        'type' => 'Think',
+        'view_path' => '',
+        'view_suffix' => 'html',
+        'strip_space' => true,
+        'view_depr' => DS,
+        'tpl_begin' => '{',
+        'tpl_end' => '}',
+        'taglib_begin' => '{',
+        'taglib_end' => '}',
+    ];
+
+    /**
+     * 架构函数
+     * @param Request $request Request对象
+     * @access public
+     */
+    public function __construct(Request $request = null)
+    {
+        // 生成request对象
+        $this->request = is_null($request) ? Request::instance() : $request;
+        // 初始化配置信息
+        $this->config = Config::get('template') ?: $this->config;
+        // 处理路由参数
+//      $route = $this->request->param('route', '');
+//      $param = explode('-', $route);
+//      $param = $this->request->param();
+        // 是否自动转换控制器和操作名
+//      $convert = \think\Config::get('url_convert');
+        $act = $this->request->dispatch()['method'][4];
+        $ctr = $this->request->dispatch()['method'][3];
+        $mod = $this->request->dispatch()['method'][2];
+        $this->action = $act ? $act : 'index';
+        $this->controller = $ctr ? $ctr : 'Index';
+        $this->addon = $mod ? $mod : 'index';
+
+        // 生成view_path
+        $view_path = 'view';
+        // 重置配置
+        Config::set('template.view_path', ADDON_PATH . $this->addon . DS . $view_path . DS);
+
+        parent::__construct($request);
+    }
+
+    /**
+     * 加载模板输出
+     * @access protected
+     * @param string $template 模板文件名
+     * @param array $vars 模板输出变量
+     * @param array $replace 模板替换
+     * @param array $config 模板参数
+     * @return mixed
+     */
+    protected function fetch($template = '', $vars = [], $replace = [], $config = [])
+    {
+        $controller = Loader::parseName($this->controller);
+        if ('think' == strtolower($this->config['type']) && $controller && 0 !== strpos($template, '/')) {
+            $depr = $this->config['view_depr'];
+            $template = str_replace(['/', ':'], $depr, $template);
+            if ('' == $template) {
+                // 如果模板文件名为空 按照默认规则定位
+                $template = str_replace('.', DS, $controller) . $depr . $this->action;
+            } elseif (false === strpos($template, $depr)) {
+                $template = str_replace('.', DS, $controller) . $depr . $template;
+            }
+        }
+        return parent::fetch($template, $vars, $replace, $config);
+    }
+}
---
+++ system/vendor/5ini99/think-addons/src/addons/Route.php
@@ -0,0 +1,57 @@
+<?php
+// +----------------------------------------------------------------------
+// | thinkphp5 Addons [ WE CAN DO IT JUST THINK IT ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2016 http://www.zzstudio.net All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: Byron Sampson <xiaobo.sun@qq.com>
+// +----------------------------------------------------------------------
+namespace think\addons;
+
+use think\Hook;
+use think\Request;
+
+/**
+ * 插件执行默认控制器
+ * Class AddonsController
+ * @package think\addons
+ */
+class Route extends Controller
+{
+    /**
+     * 插件执行
+     */
+    public function execute()
+    {
+        if (!empty($this->addon) && !empty($this->controller) && !empty($this->action)) {
+            // 获取类的命名空间
+            $class = get_addon_class($this->addon, 'controller', $this->controller);
+            if (class_exists($class)) {
+                $model = new $class();
+                if ($model === false) {
+                    abort(500, lang('addon init fail'));
+                }
+                // 调用操作
+                if (!method_exists($model, $this->action)) {
+                    abort(500, lang('Controller Class Method Not Exists'));
+                }
+                // 监听addons_init
+                Hook::listen('addons_init', $this);
+                return call_user_func_array([$model, $this->action], [Request::instance()]);
+            } else {
+                abort(500, lang('Controller Class Not Exists'));
+            }
+        }
+        if (!empty($this->addon)) {
+            abort(500, lang('插件未找到'));
+        }
+        if (!empty($this->controller)) {
+            abort(500, lang('控制器未找到'));
+        }
+        if (!empty($this->action)) {
+            abort(500, lang('方法未找到'));
+        }
+    }
+}
