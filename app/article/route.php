8484da1a6436f1237ffd509a2ae0706dda82e110
顾阳 <38082270@qq.com>
2018-04-28 18:46:38 +0800

    ..

--- app/article/route.php
+++ app/article/route.php
@@ -12,15 +12,15 @@ if (!strpos($request->url(),'Api')) {
     $categoryList = model('app\\article\\model\\Articles')->getAllCategory();
     if ($categoryList) {
         foreach ($categoryList as $key => $value) {
-            Route::rule([$value['pageRule'] => ['article/Article/index?id=' . $value["id"] . '&cid=' . $value["cid"],[],[]]]);
+            Route::rule([$value['pageRule'] => ['article/Article/index?id=' . $value["id"] . '&cid=' . $value["id"],[],[]]]);
             
             Route::rule([str_replace('.html','',$value['detailRule']).$mipInfo['urlPageBreak'].'<page>' => ['article/ArticleDetail/index',['ext'=>'html'],['id'=>'[a-zA-Z0-9_-]+']]]);
 
             Route::rule([str_replace('.html','',$value['detailRule']) => ['article/ArticleDetail/index',['ext'=>'html'],['__url__' => $value['detail__url__']],[]]]);
-            Route::rule([$value['rule'] => ['article/Article/index?id=' . $value["id"] . '&cid=' . $value["cid"],[],[]]]);
+            Route::rule([$value['rule'] => ['article/Article/index?id=' . $value["id"] . '&cid=' . $value["id"],[],[]]]);
         }
     } else {
         Route::rule(['/article/:id' => ['article/ArticleDetail/index',['ext'=>'html'],[],[]]]);
     }
-    Route::rule($modelName,'article/Article/index');
+    Route::rule('article','article/Article/index');
 }
